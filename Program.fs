57dd0d303e4122acbe0504b517c572ed4fbaaf7b
AnkitKuinkel <Ankit.Kuinkel@triosstudent.com>
2023-11-24 10:21:07 -0500

    v1

---
+++ Program.fs
@@ -0,0 +1,17 @@
+﻿// Question number 1.
+// Creating a list of 5 teams
+type Coach = {
+    Name : string
+    FormerPlayer : bool
+}
+
+type Stats = {
+    Wins : int
+    Losses : int
+}
+
+type Team = {
+    Name : string
+    Coach : Coach
+    Stats : Stats
+}
